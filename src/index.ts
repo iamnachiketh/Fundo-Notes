19e0053c7863566345d55cc3f7d2182a53c4579b
Nachiketha <nachiketh.k.c@brainsightai.com>
2025-01-03 13:02:30 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({path:'.env'});
+
+const app = express();
+
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
